b56365fd98d8bd290b89586d8f5aa70238e37674
Prasad Shelke <prasadshelke@Prasads-MacBook-Air.local>
2025-02-07 14:53:13 +0530

    3160. Find the Number of Distinct Colors Among the Balls

---
+++ leet3160.js
@@ -0,0 +1,27 @@
+// 3160. Find the Number of Distinct Colors Among the Balls
+
+/**
+ * @param {number} limit
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+var queryResults = function (limit, queries) {
+  let ans = [];
+  let ballToColor = new Map();
+  let colorCount = new Map();
+
+  for (let [ball, color] of queries) {
+    if (ballToColor.has(ball)) {
+      let prevColor = ballToColor.get(ball);
+      colorCount.set(prevColor, (colorCount.get(prevColor) || 0) - 1);
+      if (colorCount.get(prevColor) === 0) {
+        colorCount.delete(prevColor);
+      }
+    }
+    ballToColor.set(ball, color);
+    colorCount.set(color, (colorCount.get(color) || 0) + 1);
+    ans.push(colorCount.size);
+  }
+
+  return ans;
+};
