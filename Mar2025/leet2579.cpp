f052351c09f1ef8b6e5cecf4e2e4c7dc093bf06b
Prasad Shelke <shelkeprasad1102@gmail.com>
2025-03-05 09:22:33 +0530

    2579. Count Total Number of Colored Cells

---
+++ Mar2025/leet2579.cpp
@@ -0,0 +1,7 @@
+// 2579. Count Total Number of Colored Cells
+class Solution {
+ public:
+  long long coloredCells(int n) {
+    return static_cast<long>(n) * n + static_cast<long>(n - 1) * (n - 1);
+  }
+};
