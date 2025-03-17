3d54586e01ae6603645f0797428b495bde143544
Prasad Shelke <shelkeprasad1102@gmail.com>
2025-03-17 14:46:46 +0530

    2206. Divide Array Into Equal Pairs

---
+++ Mar2025/leet2206.cpp
@@ -0,0 +1,12 @@
+// 2206. Divide Array Into Equal Pairs
+class Solution {
+  public:
+   bool divideArray(vector<int>& nums) {
+     vector<int> count(501);
+ 
+     for (const int num : nums)
+       ++count[num];
+ 
+     return ranges::all_of(count, [](int c) { return c % 2 == 0; });
+   }
+ };
