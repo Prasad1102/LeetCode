09429f3af2247df8f9be0abb1077b16cc2d0fa61
Prasad Shelke <shelkeprasad1102@gmail.com>
2025-04-09 17:37:48 +0530

    3375. Minimum Operations to Make Array Values Equal to K

---
+++ Apr2025/leet3375.cpp
@@ -0,0 +1,13 @@
+// 3375. Minimum Operations to Make Array Values Equal to K
+class Solution {
+  public:
+   int minOperations(vector<int>& nums, int k) {
+     const unordered_set<int> numsSet{nums.begin(), nums.end()};
+     const int mn = ranges::min(nums);
+     if (mn < k)
+       return -1;
+     if (mn > k)
+       return numsSet.size();
+     return numsSet.size() - 1;
+   }
+ };
