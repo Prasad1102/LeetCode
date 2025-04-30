d2d03062552fc897067c8fae255e6d211ece59a0
Prasad Shelke <shelkeprasad1102@gmail.com>
2025-04-30 11:49:22 +0530

    1295. Find Numbers with Even Number of Digits

---
+++ Apr2025/leet1295.cpp
@@ -0,0 +1,13 @@
+// 1295. Find Numbers with Even Number of Digits
+class Solution {
+  public:
+   int findNumbers(vector<int>& nums) {
+     int ans = 0;
+ 
+     for (int num : nums)
+       if (9 < num && num < 100 || 999 < num && num < 10000 || num == 100000)
+         ++ans;
+ 
+     return ans;
+   }
+ };
